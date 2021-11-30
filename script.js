6ca9e6ac300f6f872a23ce08f57e4817c42632cf
berkaykarlik <brkykrlk@gmail.com>
2021-12-01 00:02:21 +0300

    ADD: floating point numbers

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const result_div = document.querySelector('#result');
 const all_numbers = document.querySelectorAll('.number');
 const all_ops = document.querySelectorAll('.operator');
 const clear_button = document.querySelector('#clear');
+const point_button = document.querySelector('#point');
+
+const DECIMAL_PLACES = 6;
 
 function _throw(m) { throw m; } //for throwing exception inside ternary op
+
+Number.prototype.countDecimals = function () {
+    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
+    return this.toString().split(".")[1].length || 0;
+}
 
 const add = (x,y) => x+y ;
 const substract = (x,y) => x-y ;
@@ -23,22 +31,39 @@ let curr_number = null;
 let answer = null;
 let chosen_symbol = null;
 let clean_after_op = false;
+let floating_point = false;
+let floating_zeros = 0;
 
-
-function updateOperationDiv(putEqual){
+function updateOperationDiv(putEqual,addZeros2Curr){
     let text = chosen_symbol  !== null ? old_number + chosen_symbol : '';
-    text += curr_number !== null ?  curr_number : '' ;
+    text += curr_number !== null ?  curr_number + addZeros2Curr : '' ;
     text += putEqual ? '=' : '';
     op_div.textContent = text;
     result_div.textContent = answer;
 }
 
 function updateCurrNumber(val){
-    curr_number = curr_number !== null ? parseInt(val) + curr_number * 10 : parseInt(val);
+    let addZeros2Curr = '';
+    if (floating_point){
+        if (curr_number.countDecimals() == DECIMAL_PLACES){
+            return;
+        }
+        if(val == 0){
+            floating_zeros += 1
+            addZeros2Curr = curr_number % 1 === 0 ?'.' + ('0'.repeat(floating_zeros )) : ('0'.repeat(floating_zeros )) ;
+        }
+        else{
+        curr_number = curr_number !== null ? parseFloat(( parseInt(val) / Math.pow(10,(curr_number.countDecimals()+floating_zeros+ 1))+ curr_number).toFixed(DECIMAL_PLACES)) : parseInt(val) / 10;
+        floating_zeros = 0;
+    }
+    }
+    else{
+        curr_number = curr_number !== null ? parseInt(val) + curr_number * 10 : parseInt(val);
+    }
     if (answer){
         answer = null;
     }
-    updateOperationDiv();
+    updateOperationDiv(false,addZeros2Curr);
 }
 
 function updateOperation(symbol){
@@ -53,11 +78,14 @@ function updateOperation(symbol){
                 return;
             }
             if (answer % 1 !== 0){
-                answer = answer.toFixed(2);
+                answer = parseFloat(answer.toFixed(DECIMAL_PLACES));
             }
-            updateOperationDiv(true);
+            updateOperationDiv(true,'');
             old_number = null;
             curr_number = null;
+            floating_point = false;
+            floating_zeros = 1;
+            point_button.disabled =false
             chosen_symbol = null;
         }
     }
@@ -71,10 +99,13 @@ function updateOperation(symbol){
                 return;
             }
             if (answer % 1 !== 0){
-                answer = answer.toFixed(2);
+                answer = parseFloat(answer.toFixed(DECIMAL_PLACES));
             }
-            updateOperationDiv(true);
+            updateOperationDiv(true,'');
             curr_number = null;
+            floating_point = false;
+            floating_zeros = 1;
+            point_button.disabled =false
             chosen_symbol = symbol;
             old_number = answer;
         }
@@ -82,13 +113,16 @@ function updateOperation(symbol){
             chosen_symbol = symbol;
             old_number = answer;
             curr_number = null;
-            updateOperationDiv(false);
+            updateOperationDiv(false,'');
         }
         else if(curr_number !== null){
             chosen_symbol = symbol;
             old_number = curr_number;
             curr_number = null;
-            updateOperationDiv(false);
+            floating_point = false;
+            floating_zeros = 1;
+            point_button.disabled =false;
+            updateOperationDiv(false,'');
         }
     }
 }
@@ -103,4 +137,9 @@ all_ops.forEach(operator_button => {
 
 clear_button.addEventListener('click', () => {
     location.reload(); //reload page
+});
+
+point_button.addEventListener('click', () => {
+    floating_point = true;
+    point_button.disabled =true;
 });
